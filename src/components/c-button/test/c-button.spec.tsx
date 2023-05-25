1d2e83f91988f31d0a0c63724d62fcff0a1655e1
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-05-25 11:51:40 +0300

    c-button unit-test part 2

--- src/components/c-button/test/c-button.spec.tsx
+++ src/components/c-button/test/c-button.spec.tsx
@@ -95,4 +95,70 @@ describe('button', () => {
 
     expect(page.root).toMatchSnapshot();
   });
+
+  it('inverted button style for dark mode', async () => {
+    const page = await newSpecPage({
+      components: [CButton],
+      html: `<c-button inverted={true}>
+      ButtonText
+      </c-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('loading', async () => {
+    const page = await newSpecPage({
+      components: [CButton],
+      html: `<c-button loading={true}>
+      ButtonText
+      </c-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('remove border', async () => {
+    const page = await newSpecPage({
+      components: [CButton],
+      html: `<c-button no-radius={true}>
+      ButtonText
+      </c-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('with outline', async () => {
+    const page = await newSpecPage({
+      components: [CButton],
+      html: `<c-button outlined={true}>
+      ButtonText
+      </c-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('with path to svg-icon', async () => {
+    const page = await newSpecPage({
+      components: [CButton],
+      html: `<c-button path="path to my svg">
+      ButtonText
+      </c-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('with path to svg-icon and icon after text', async () => {
+    const page = await newSpecPage({
+      components: [CButton],
+      html: `<c-button path="path to my svg" icon-end={true}>
+      ButtonText
+      </c-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
 });
