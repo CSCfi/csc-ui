9346ebf353e3172a3f99369875dd3e80a6b6c590
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-05-29 10:21:31 +0300

    add missing <-marks

--- src/components/c-card/test/c-card.spec.tsx
+++ src/components/c-card/test/c-card.spec.tsx
@@ -5,7 +5,7 @@ describe('c-card', () => {
   it('renders default html', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card></c-card`,
+      html: `<c-card></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -14,13 +14,16 @@ describe('c-card', () => {
   it('render default with text', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card>
-    <c-card-title>The title</c-card-title>
-    <c-card-content>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.</c-card-content>
-      </c-card`,
+      html: `
+      <c-card>
+        <c-card-title>The title</c-card-title>
+        <c-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </c-card-content>
+      </c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -29,7 +32,7 @@ describe('c-card', () => {
   it('render custom background-color', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card background-color="red"></c-card`,
+      html: `<c-card background-color="red"></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -38,7 +41,7 @@ describe('c-card', () => {
   it('render chosen background-photo, called allas', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card background="allas"></c-card`,
+      html: `<c-card background="allas"></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -47,7 +50,7 @@ describe('c-card', () => {
   it('render another chosen background-photo, called mahti', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card background="mahti"></c-card`,
+      html: `<c-card background="mahti"></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -56,7 +59,7 @@ describe('c-card', () => {
   it('render custom background-photo', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card background="dog"></c-card`,
+      html: `<c-card background="dog"></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -65,7 +68,7 @@ describe('c-card', () => {
   it('with fullscreen button', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card fullscreen={true}></c-card`,
+      html: `<c-card fullscreen={true}></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -74,7 +77,7 @@ describe('c-card', () => {
   it('opens fullscreen when button pressed', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card fullscreen={true}></c-card`,
+      html: `<c-card fullscreen={true}></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
@@ -93,7 +96,7 @@ describe('c-card', () => {
   it('shrinks back to normal when button pressed in fullscreen-mode', async () => {
     const page = await newSpecPage({
       components: [CCard],
-      html: `<c-card fullscreen={true}></c-card`,
+      html: `<c-card fullscreen={true}></c-card>`,
     });
 
     expect(page.root).toMatchSnapshot();
