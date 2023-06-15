2f291f6e4b1e550dc87887896ad1551223d70a3f
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-06-15 10:18:18 +0300

    c-login-card unit-test, add title, content, actions

--- src/components/c-login-card/test/c-login-card.spec.tsx
+++ src/components/c-login-card/test/c-login-card.spec.tsx
@@ -1,5 +1,8 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { CLoginCard } from '../c-login-card';
+import { CLoginCardTitle } from '../c-login-card-title';
+import { CLoginCardContent } from '../c-login-card-content';
+import { CLoginCardActions } from '../c-login-card-actions';
 
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
@@ -8,12 +11,104 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
 }));
 
 describe('c-login-card', () => {
+  const title = `Login title`;
   const content = `This is a login card`;
   it('renders default html', async () => {
     const page = await newSpecPage({
       components: [CLoginCard],
       html: `<c-login-card>
       ${content}
+      </c-login-card>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('renders title', async () => {
+    const page = await newSpecPage({
+      components: [CLoginCard, CLoginCardTitle],
+      html: `
+      <c-login-card>
+        <c-login-card-title>
+          ${title}
+        </c-login-card-title>
+      </c-login-card>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('renders content', async () => {
+    const page = await newSpecPage({
+      components: [CLoginCard, CLoginCardContent],
+      html: `
+      <c-login-card>
+        <c-login-card-content>
+         ${content}
+        </c-login-card-content>
+      </c-login-card>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('renders actions', async () => {
+    const page = await newSpecPage({
+      components: [CLoginCard, CLoginCardActions],
+      html: `
+      <c-login-card>
+        <c-login-card-actions>
+        </c-login-card-actions>
+      </c-login-card>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('renders title, content, actions', async () => {
+    const page = await newSpecPage({
+      components: [
+        CLoginCard,
+        CLoginCardTitle,
+        CLoginCardContent,
+        CLoginCardActions,
+      ],
+      html: `
+      <c-login-card>
+        <c-login-card-title>
+          ${title}
+        </c-login-card-title>
+        <c-login-card-content>
+          ${content}
+        </c-login-card-content>
+        <c-login-card-actions>
+        </c-login-card-actions>
+      </c-login-card>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('renders actions, custom align: end', async () => {
+    const page = await newSpecPage({
+      components: [CLoginCard, CLoginCardActions],
+      html: `
+      <c-login-card>
+        <c-login-card-actions align="end">
+        </c-login-card-actions>
+      </c-login-card>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('renders actions, custom justification: space-around', async () => {
+    const page = await newSpecPage({
+      components: [CLoginCard, CLoginCardActions],
+      html: `
+      <c-login-card>
+        <c-login-card-actions justify="space-around">
+        </c-login-card-actions>
       </c-login-card>`,
     });
 
