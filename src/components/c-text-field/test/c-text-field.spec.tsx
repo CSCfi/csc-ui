44b7be370914ab4c3622c5c06a0d8d243ccef1a3
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-06-19 13:22:21 +0300

    start of c-text-field unit-test

---
+++ src/components/c-text-field/test/c-text-field.spec.tsx
@@ -0,0 +1,13 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { CTextField } from '../c-text-field';
+
+describe('c-text-field', () => {
+  it('renders default html', async () => {
+    const page = await newSpecPage({
+      components: [CTextField],
+      html: `<c-text-field>Here is text</c-text-field>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+});
