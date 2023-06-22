267629ff82160ae27e7b84e7c9ef2471760d10fc
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-06-22 14:34:36 +0300

    c-checkbox unit-test

--- src/components/c-checkbox/test/c-checkbox.spec.tsx
+++ src/components/c-checkbox/test/c-checkbox.spec.tsx
@@ -10,4 +10,40 @@ describe('c-checkbox', () => {
 
     expect(page.root).toMatchSnapshot();
   });
+
+  it('renders hint message', async () => {
+    const page = await newSpecPage({
+      components: [CCheckbox],
+      html: `<c-checkbox hint="Check this out"></c-checkbox>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('hides hint message', async () => {
+    const page = await newSpecPage({
+      components: [CCheckbox],
+      html: `<c-checkbox hide-details hint="Check this out"></c-checkbox>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('render indeterminate', async () => {
+    const page = await newSpecPage({
+      components: [CCheckbox],
+      html: `<c-checkbox indeterminate=${true}></c-checkbox>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('renders label', async () => {
+    const page = await newSpecPage({
+      components: [CCheckbox],
+      html: `<c-checkbox label="This is a label"></c-checkbox>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
 });
