a1668ca8a0aee62ce014d6ab027038bc6ee141cb
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-06-28 08:26:53 +0300

    e2e testing in progress

--- src/components/c-tab-buttons/test/c-tab-buttons.e2e.tsx
+++ src/components/c-tab-buttons/test/c-tab-buttons.e2e.tsx
@@ -1,9 +1,30 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('c-tab-buttons', () => {
-  it('reacts to value change', async () => {
+  it('can not be checked when disabled', async () => {
     const page = await newE2EPage({
-      html: `<c-tab-buttons></c-tab-buttons>`,
+      html: `
+      <c-tab-buttons>
+      <c-button></c-button>
+      <c-button></c-button>
+      <c-button></c-button>
+      </c-tab-buttons>
+      `,
     });
+
+    const tabButtons = await page.find('c-tab-buttons');
+
+    tabButtons.setProperty('disabled', true);
+
+    await page.waitForChanges();
+
+    const buttons = await page.findAll('c-button >>> .c-button-active');
+    expect(buttons.length).toBe(null);
+
+    // await tabButtons.click();
+
+    // await page.waitForChanges();
+
+    // expect(checkMark).toBe(null);
   });
 });
