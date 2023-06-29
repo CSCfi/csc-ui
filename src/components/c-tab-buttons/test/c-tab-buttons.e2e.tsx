920f3e91718be9166632d64179542649018192f1
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-06-29 16:03:16 +0300

    c-tab-buttons e2e-test in progress

--- src/components/c-tab-buttons/test/c-tab-buttons.e2e.tsx
+++ src/components/c-tab-buttons/test/c-tab-buttons.e2e.tsx
@@ -1,30 +1,54 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('c-tab-buttons', () => {
-  it('can not be checked when disabled', async () => {
+  it('one selection is mandatory', async () => {
     const page = await newE2EPage({
       html: `
       <c-tab-buttons>
-      <c-button></c-button>
-      <c-button></c-button>
-      <c-button></c-button>
+      <c-button>One</c-button>
+      <c-button>Two</c-button>
+      <c-button>Three</c-button>
       </c-tab-buttons>
       `,
     });
 
-    const tabButtons = await page.find('c-tab-buttons');
-
-    tabButtons.setProperty('disabled', true);
+    const tab = await page.find('c-tab-button');
+    tab.setProperty('value', 0);
 
     await page.waitForChanges();
 
-    const buttons = await page.findAll('c-button >>> .c-button-active');
-    expect(buttons.length).toBe(null);
+    const buttons = await page.findAll('c-button');
+    let activeButtons = await page.findAll('.c-button--active');
 
-    // await tabButtons.click();
+    expect(activeButtons.length).toBe(1);
+
+    await buttons[0].click();
+
+    await page.waitForChanges();
+
+    activeButtons = await page.findAll('.c-button--active');
+
+    expect(activeButtons.length).toBe(0);
+
+    //expect(page).toMatchSnapshot();
+
+    // tabButtons = await page.find('c-tab-buttons');
+    // tabButtons.setProperty('mandatory', true);
 
     // await page.waitForChanges();
 
-    // expect(checkMark).toBe(null);
+    // buttons = await page.findAll('c-button');
+
+    // await buttons[1].click();
+
+    // await page.waitForChanges();
+
+    // expect(page).toMatchSnapshot();
+
+    // await buttons[1].click();
+
+    // await page.waitForChanges();
+
+    // expect(page).toMatchSnapshot();
   });
 });
