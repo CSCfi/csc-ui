108d549c58e18591840fd15d1b96cc9432923c91
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-07-13 10:07:36 +0300

    c-tab-buttons e2e test

--- src/components/c-tab-buttons/test/c-tab-buttons.e2e.tsx
+++ src/components/c-tab-buttons/test/c-tab-buttons.e2e.tsx
@@ -1,6 +1,29 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('c-tab-buttons', () => {
+  it('basic  interaction test: clicking buttons active and non-active', async () => {
+    const page = await newE2EPage({
+      html: `
+      <c-tab-buttons>
+      <c-button>One</c-button>
+      <c-button>Two</c-button>
+      <c-button>Three</c-button>
+      </c-tab-buttons>
+      `,
+    });
+    const tab = await page.find('c-tab-buttons');
+    tab.setProperty('value', 0);
+
+    const buttons = await page.findAll('c-button');
+    expect(buttons[0]).toHaveClass('c-button--active');
+
+    buttons[0].click();
+
+    await page.waitForChanges();
+
+    expect(buttons[0]).not.toHaveClass('c-button--active');
+  });
+
   it('one selection is mandatory', async () => {
     const page = await newE2EPage({
       html: `
@@ -73,5 +96,48 @@ describe('c-tab-buttons', () => {
     activeButtons = await page.findAll('.c-button--active');
 
     expect(activeButtons.length).toBe(1);
+  });
+
+  it('interaction disabled', async () => {
+    const page = await newE2EPage({
+      html: `
+      <c-tab-buttons>
+      <c-button>One</c-button>
+      <c-button>Two</c-button>
+      <c-button>Three</c-button>
+      </c-tab-buttons>
+      `,
+    });
+
+    // set the disabled-class to parent
+    const tab = await page.find('c-tab-buttons');
+    tab.setAttribute('disabled', true);
+    tab.setProperty('value', 0);
+
+    await page.waitForChanges();
+
+    // make first button active, but when user tries to interact, it stays active
+    const buttons = await page.findAll('c-button');
+    expect(buttons[0]).toHaveClass('c-button--active');
+
+    buttons[0].click();
+
+    await page.waitForChanges();
+
+    expect(buttons[0]).toHaveClass('c-button--active');
+
+    // make two others not-active, and they don't turn active when user tries to interact
+
+    buttons[1].click();
+
+    await page.waitForChanges();
+
+    expect(buttons[1]).not.toHaveClass('c-button--active');
+
+    buttons[2].click();
+
+    await page.waitForChanges();
+
+    expect(buttons[2]).not.toHaveClass('c-button--active');
   });
 });
