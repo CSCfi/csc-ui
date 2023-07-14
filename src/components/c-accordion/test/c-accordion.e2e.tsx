fcc9ef35af0883a56b97f49ad8dfce877c2dd9b0
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-07-14 12:32:39 +0300

    .

--- src/components/c-accordion/test/c-accordion.e2e.tsx
+++ src/components/c-accordion/test/c-accordion.e2e.tsx
@@ -1,10 +1,9 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('c-accordion', () => {
-  it('disallows collapsing all items', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent(`
+  it('one selection mandatory', async () => {
+    const page = await newE2EPage({
+      html: `
     <c-accordion>
         <c-accordion-item>
             <i class="mdi mdi-access-point" slot="icon"></i>
@@ -19,25 +18,11 @@ describe('c-accordion', () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum perspiciatis
             itaque!
         </c-accordion-item>
-  </c-accordion>`);
+  </c-accordion>`,
+    });
 
-    // const expandedItem = await page.find(
-    //   'c-accordion >>> c-accordion-item--expanded',
-    // );
-    // expect(expandedItem).toBe(null);
+    const accordionItems = await page.findAll('c-accordion > c-accordion-item');
 
-    // const accordion = await page.find('c-accordion');
-    // accordion.setProperty('mandatory', true);
-    // accordion.setProperty('value', 'one');
-
-    // await page.waitForChanges();
-
-    const accordionItems = await page.findAll(
-      'c-accordion >>> c-accordion-item',
-    );
-
-    await page.waitForChanges();
-
-    expect(accordionItems.length).toBe(2);
+    expect(accordionItems.length).toBe(3);
   });
 });
