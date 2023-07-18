8328458f773dd0d36d712778d340efe6a5fe1deb
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-07-18 12:37:00 +0300

    unit-test fixing

--- src/components/c-sidenavigation/test/c-sidenavigation.spec.tsx
+++ src/components/c-sidenavigation/test/c-sidenavigation.spec.tsx
@@ -1,32 +1,20 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { CSidenavigation } from '../c-sidenavigation';
+import { CSidenavigationitem } from '../../c-sidenavigationitem/c-sidenavigationitem';
+import { CSubnavigationitem } from '../../c-subnavigationitem/c-subnavigationitem';
 
 describe('c-sidenavigation', () => {
-  it('renders default html', async () => {
-    const page = await newSpecPage({
-      components: [CSidenavigation],
-      html: `<c-sidenavigation>
-     </c-sidenavigation>`,
-    });
-
-    expect(page.root).toMatchSnapshot();
-  });
-
   it('renders visible menu', async () => {
     const page = await newSpecPage({
-      components: [CSidenavigation],
-      html: `<c-sidenavigation menu-visible=${true}>
-     </c-sidenavigation>`,
-    });
-
-    expect(page.root).toMatchSnapshot();
-  });
-
-  it('renders visible mobile-size-menu', async () => {
-    const page = await newSpecPage({
-      components: [CSidenavigation],
-      html: `<c-sidenavigation mobile=${true}>
-     </c-sidenavigation>`,
+      components: [CSidenavigation, CSidenavigationitem, CSubnavigationitem],
+      html: `
+      <c-sidenavigation menu-visible=${true}>
+        <c-sidenavigationitem active href="google.com" target="_blank">Google</c-sidenavigationitem>
+        <c-sidenavigationitem> Something
+          <c-subnavigationitem focusable href="somewhere.com" target="_blank"></c-subnavigationitem>
+        </c-sidenavigationitem>
+        <c-sidenavigationitem href="csc.fi" target="_blank">CSC</c-sidenavigationitem>
+      </c-sidenavigation>`,
     });
 
     expect(page.root).toMatchSnapshot();
