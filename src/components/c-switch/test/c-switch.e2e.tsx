d1368dda7fa3a99dd4235bf5fa4f1c10f905b314
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-07-20 13:58:21 +0300

    c-switch e2e

---
+++ src/components/c-switch/test/c-switch.e2e.tsx
@@ -0,0 +1,75 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('c-switch', () => {
+  it('reacts to value change', async () => {
+    const page = await newE2EPage({
+      html: `<c-switch></c-switch>`,
+    });
+
+    let checkMark = await page.find('c-switch >>> input');
+    expect(checkMark).not.toHaveAttribute('aria-checked');
+
+    const slider = await page.find('c-switch');
+
+    slider.setAttribute('value', true);
+
+    await page.waitForChanges();
+
+    checkMark = await page.find('c-switch >>> input');
+
+    expect(checkMark).toHaveAttribute('aria-checked');
+  });
+
+  it('value changes on click', async () => {
+    const page = await newE2EPage({
+      html: `<c-switch></c-switch>`,
+    });
+
+    let checkMark = await page.find('c-switch >>> input');
+    expect(checkMark).not.toHaveAttribute('aria-checked');
+
+    const slider = await page.find('c-switch');
+
+    slider.click();
+
+    await page.waitForChanges();
+
+    checkMark = await page.find('c-switch >>> input');
+
+    expect(checkMark).toHaveAttribute('aria-checked');
+  });
+
+  it('can not be clicked when disabled', async () => {
+    const page = await newE2EPage({
+      html: `<c-switch></c-switch>`,
+    });
+
+    let checkMark = await page.find('c-switch >>> input');
+    expect(checkMark).not.toHaveAttribute('aria-checked');
+
+    const slider = await page.find('c-switch');
+
+    slider.setAttribute('disabled', true);
+
+    await page.waitForChanges();
+
+    slider.click();
+
+    await page.waitForChanges();
+
+    checkMark = await page.find('c-switch >>> input');
+
+    expect(checkMark).not.toHaveAttribute('aria-checked');
+  });
+
+  // problem with detecting the text
+  it.skip('require selection', async () => {
+    const page = await newE2EPage({
+      html: `<c-switch required>Required</c-switch>`,
+    });
+
+    const requiredText = await page.find('c-switch >>> .c-switch__label');
+
+    expect(requiredText.innerText).toContain('Required');
+  });
+});
