d24ff409e89f1a5ccb554050c2d3ed046d1a614d
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-07-21 13:32:25 +0300

    e2e

--- src/components/c-accordion/test/c-accordion.e2e.tsx
+++ src/components/c-accordion/test/c-accordion.e2e.tsx
@@ -1,7 +1,7 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('c-accordion', () => {
-  it('render default', async () => {
+  it('reacts to expansion change', async () => {
     const page = await newE2EPage({
       html: `
     <c-accordion>
@@ -21,19 +21,60 @@ describe('c-accordion', () => {
   </c-accordion>`,
     });
 
-    const accordionItems = await page.findAll('c-accordion > c-accordion-item');
+    const accordion = await page.find('c-accordion');
+    accordion.setProperty('value', 1);
 
-    expect(accordionItems.length).toBe(3);
+    await page.waitForChanges();
 
-    accordionItems[1].click();
+    const accordionItems = await page.findAll('c-accordion-item');
+    accordionItems[0].setProperty('value', 1);
+    accordionItems[1].setProperty('value', 2);
+    accordionItems[2].setProperty('value', 3);
+
+    await page.waitForChanges();
+
+    accordionItems[1].setProperty('expanded', true);
+
+    await page.waitForChanges();
+
+    expect(page).toMatchSnapshot();
+  });
+
+  it('reacts to click', async () => {
+    const page = await newE2EPage({
+      html: `
+    <c-accordion>
+        <c-accordion-item>
+            <i class="mdi mdi-access-point" slot="icon"></i>
+            Lorem ipsum dolor sit amet.
+        </c-accordion-item>
+        <c-accordion-item>
+            <i class="mdi mdi-nas" slot="icon"></i>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, perspiciatis?
+        </c-accordion-item>
+        <c-accordion-item>
+            <i class="mdi mdi-camera" slot="icon"></i>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum perspiciatis
+            itaque!
+        </c-accordion-item>
+  </c-accordion>`,
+    });
+
+    const accordion = await page.find('c-accordion');
+    accordion.setProperty('value', 1);
+
+    await page.waitForChanges();
+
+    const accordionItems = await page.findAll('c-accordion-item');
+    accordionItems[0].setProperty('value', 1);
+    accordionItems[1].setProperty('value', 2);
+    accordionItems[2].setProperty('value', 3);
 
     await page.waitForChanges();
 
     accordionItems[1].click();
 
     await page.waitForChanges();
-
-    // problem -- element that should indicate if the item is open or closed stays expanded
 
     expect(page).toMatchSnapshot();
   });
